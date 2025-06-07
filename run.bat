f9bb0d1e86ae975d06ee0f51e454f882832db476
cbx-nz <christmastvonlineclothes@gmail.com>
2025-06-07 17:40:09 +1200

    first commit

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Change directory to script folder
+cd /d "%~dp0"
+
+REM Run the server.js with Node.js
+"C:\Program Files\nodejs\node.exe" server.js
+
+REM Optional: Pause to see errors if run manually
+REM pause
+git add .
+git commit -m "data updated"
+git push origin main
+REM End of script
